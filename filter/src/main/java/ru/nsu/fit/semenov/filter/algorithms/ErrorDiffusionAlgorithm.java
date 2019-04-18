d5acb3d364c2c682e90930237f00ef76818b66e2
Sergey Semenov <lvfx00@gmail.com>
2019-04-18 13:02:56 +0700

    Fixed error diffusion algorithm

--- filter/src/main/java/ru/nsu/fit/semenov/filter/algorithms/ErrorDiffusionAlgorithm.java
+++ filter/src/main/java/ru/nsu/fit/semenov/filter/algorithms/ErrorDiffusionAlgorithm.java
@@ -1,52 +1,49 @@
 package ru.nsu.fit.semenov.filter.algorithms;
 
 import org.jetbrains.annotations.NotNull;
+import ru.nsu.fit.semenov.filter.util.ImageUtils;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-import static ru.nsu.fit.semenov.filter.util.ImageUtils.COLOR_COMPONENTS_NUM;
-import static ru.nsu.fit.semenov.filter.util.ImageUtils.isIndexValid;
+import static ru.nsu.fit.semenov.filter.util.ImageUtils.*;
 
 public class ErrorDiffusionAlgorithm implements Algorithm {
-    private static final ErrorDistribution[] ERROR_DISTRIBUTIONS = {
-            new ErrorDistribution(1, 0, 0.4375f),
-            new ErrorDistribution(-1, 1, 0.1875f),
-            new ErrorDistribution(0, 1, 0.3125f),
-            new ErrorDistribution(1, 1, 0.0625f),
+    private static final ErrorDistr[] ERROR_DISTRIBUTIONS = {
+            new ErrorDistr(1, 0, 7f / 16),
+            new ErrorDistr(-1, 1, 3f / 16),
+            new ErrorDistr(0, 1, 5f / 16),
+            new ErrorDistr(1, 1, 1f / 16),
     };
+
     private final float[] paletteSteps = new float[COLOR_COMPONENTS_NUM];
 
     public ErrorDiffusionAlgorithm(int redPaletteSize, int greenPaletteSize, int bluePaletteSize) {
         final int[] sizes = {redPaletteSize, greenPaletteSize, bluePaletteSize};
         for (int i = 0; i < COLOR_COMPONENTS_NUM; ++i) {
-            paletteSteps[i] = 1f / (sizes[i] - 1);
+            paletteSteps[i] = (float) MAX_COLOR_VALUE / (sizes[i] - 1);
         }
     }
 
     @Override
     public @NotNull BufferedImage apply(@NotNull BufferedImage sourceImage) {
+        final int[][][] colorComponents = ImageUtils.extractColorComponents(sourceImage);
         BufferedImage resultImage = new BufferedImage(
                 sourceImage.getWidth(),
                 sourceImage.getHeight(),
                 sourceImage.getType()
         );
-        float[][][] errors = new float[COLOR_COMPONENTS_NUM][sourceImage.getWidth()][sourceImage.getHeight()];
-        // TODO fix error
         for (int x = 0; x < sourceImage.getWidth(); ++x) {
             for (int y = 0; y < sourceImage.getHeight(); ++y) {
-                float[] oldColor = new float[COLOR_COMPONENTS_NUM];
-                new Color(sourceImage.getRGB(x, y)).getColorComponents(oldColor);
-                float[] newColor = new float[COLOR_COMPONENTS_NUM];
+                int[] newColor = new int[COLOR_COMPONENTS_NUM];
                 for (int i = 0; i < COLOR_COMPONENTS_NUM; ++i) {
-                    newColor[i] = findClosestInPalette(paletteSteps[i], oldColor[i] + errors[i][x][y]);
-                    float errorValue = oldColor[i] - newColor[i];
-                    for (ErrorDistribution errorDistribution :
-                            ERROR_DISTRIBUTIONS) {
-                        int errorX = x + errorDistribution.getxOffset();
-                        int errorY = y + errorDistribution.getyOffset();
+                    newColor[i] = findClosestInPalette(colorComponents[x][y][i], paletteSteps[i]);
+                    int error = colorComponents[x][y][i] - newColor[i];
+                    for (ErrorDistr errorDistr : ERROR_DISTRIBUTIONS) {
+                        int errorX = x + errorDistr.getxOffset();
+                        int errorY = y + errorDistr.getyOffset();
                         if (isIndexValid(errorX, errorY, sourceImage.getWidth(), sourceImage.getHeight())) {
-                            errors[i][errorX][errorY] += errorDistribution.getFactor() * errorValue;
+                            colorComponents[errorX][errorY][i] += Math.round(errorDistr.getFactor() * error);
                         }
                     }
                 }
@@ -56,23 +53,20 @@ public class ErrorDiffusionAlgorithm implements Algorithm {
         return resultImage;
     }
 
-    private static float findClosestInPalette(float paletteStep, float color) {
-        float steps = color / paletteStep;
-        float result;
-        if (steps - (int) steps > 0.5f) {
-            result = ((int) steps + 1) * paletteStep;
-        } else {
-            result = ((int) steps) * paletteStep;
-        }
+    private static int findClosestInPalette(int color, float paletteStep) {
+        int stepsNum = Math.round(color / paletteStep);
+        int result = Math.round(stepsNum * paletteStep);
+        if (result > MAX_COLOR_VALUE) return MAX_COLOR_VALUE;
+        if (result < 0) return 0;
         return result;
     }
 
-    private static class ErrorDistribution {
+    private static class ErrorDistr {
         private final int xOffset;
         private final int yOffset;
         private final float factor;
 
-        public ErrorDistribution(int xOffset, int yOffset, float factor) {
+        public ErrorDistr(int xOffset, int yOffset, float factor) {
             this.xOffset = xOffset;
             this.yOffset = yOffset;
             this.factor = factor;
